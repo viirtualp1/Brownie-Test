679f3a185b793187731b6dfa07a37ad6dda86bb6
Nikita <zininnikita309@gmail.com>
2021-11-07 17:37:30 +0900

    start make triggers

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -11,9 +11,6 @@ public class Hero : MonoBehaviour
     // Лист собранных предметов (!добавлять id предметов, чтобы потом было их легко удалить!)
     public static List<string> items;
     public static List<string> collectsCards;
-    
-    // Нужные предметы для игры
-    public static List<string> neededItems;
 
     // Приватные поля
     [SerializeField] private float speed = 3f; // Скорость движения
@@ -21,6 +18,9 @@ public class Hero : MonoBehaviour
 
     // Приватные переменные
     private bool isGrounded = false;
+
+    public GameObject cactus;
+    public Sprite cactus_new;
 
     // Ссылки
     private Rigidbody2D rb;
@@ -68,7 +68,8 @@ public class Hero : MonoBehaviour
 
     private float time = 2f;
 
-    private int day = 1;
+    public static int day = 1;
+    
     public Sprite TVOnSprite;
     public Sprite TVOffSprite;
     private bool isRCB = false;
@@ -99,6 +100,33 @@ public class Hero : MonoBehaviour
     private int collectsCardsCount;
     private int itemsCount;
 
+    public GameObject stickyNote_o;
+
+    private GameObject cardObject;
+
+    private Collider2D wire;
+    private bool isWire = false;
+
+    private int wireCounter = 0;
+    
+    private string currentTaskString;
+
+    // Касается ли игрок инструментов
+    private bool isInstruments = false;
+
+    // Взял ли игрок отвертку
+    private bool isTakeInstuments = false;
+
+    // Касается ли игрок майнинг фермы
+    private bool isMining = false;
+
+    private Collider2D mining;
+
+    private Collider2D light;
+    private bool isLightOff = false;
+    private bool isLightTrigger = false;
+    public Sprite newLight;
+
     private States State
     {
         get { return (States)anim.GetInteger("state"); }
@@ -109,9 +137,59 @@ public class Hero : MonoBehaviour
     void Start()
     {
         getSaves();
+
+        if (SceneManager.GetActiveScene().name == "HomeBooba")
+        {
+            for (int i = 0; i < collectsCards.Count; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    cardObject = GameObject.Find("card" + j);
+                    cardObject.GetComponent<SpriteRenderer>().color = Color.white;
+                }
+            }
+        }
+
+        for (int i = 0; i < collectsCards.Count; i++)
+        {
+            if (GameObject.Find(collectsCards[i]))
+                Destroy(GameObject.Find(collectsCards[i]));
+        }
+
+        if (day >= 3)
+            cactus.GetComponent<SpriteRenderer>().sprite = cactus_new;
+        if (day > 1)
+            Destroy(stickyNote_o);
+        if (day < 2)
+            GameObject.Find("stairsToLivingRoom").GetComponent<BoxCollider2D>().enabled = false;
+        if (day < 3)
+            GameObject.Find("DoorToGarage").GetComponent<BoxCollider2D>().enabled = false;
+        if (day < 4)
+            GameObject.Find("DoorToBathRoom").GetComponent<BoxCollider2D>().enabled = false;
+        if (day < 5)
+            GameObject.Find("DoorToKitchen").GetComponent<BoxCollider2D>().enabled = false;
+        if (day < 6)
+            GameObject.Find("DoorToRoof").GetComponent<BoxCollider2D>().enabled = false;
+
+        currentTask();
+
         GetOrDupe.GetComponent<Canvas>().enabled = false;
         training.GetComponent<Canvas>().enabled = false;
         items = new List<string>();
+    }
+
+    private void currentTask()
+    {
+        if (day == 2)
+            currentTaskString = "Гостиная";
+        else if (day == 3)
+            currentTaskString = "Гараж";
+        else if (day == 4)
+            currentTaskString = "Ванная";
+        else if (day == 5)
+            currentTaskString = "Кухня";
+        else if (day == 6)
+            currentTaskString = "Крыша";
     }
 
     private void Awake()
@@ -128,14 +206,16 @@ public class Hero : MonoBehaviour
 
     private void Update()
     {
+        // Если Буба стоит на земле => анимация idle
         if (isGrounded) State = States.idle;
 
+        // Ходьба и прыжки Бубы
         if (isBoobaCanMove && Input.GetButton("Horizontal"))
             Run();
         if (isBoobaCanJump && isGrounded && Input.GetKeyDown(KeyCode.Space))
             Jump();
 
-        // Triggered for sock
+        // Подбираем N предмет
         if (isTriggeredCollectable && isCollectable && Input.GetKeyDown(KeyCode.X))
         {
             string itemType = item.gameObject.GetComponent<CollectableScript>().itemType;
@@ -144,7 +224,7 @@ public class Hero : MonoBehaviour
             Destroy(item.gameObject);
         }
 
-        // Triggered for carts
+        // Подбираем карточку
         if (isCardTake && Input.GetKeyDown(KeyCode.X))
         {   
             isCardTake = false;
@@ -162,9 +242,9 @@ public class Hero : MonoBehaviour
             GetOrDupe.GetComponent<Canvas>().enabled = true;
         }
 
+        // Открываем стикер с обучением
         if (!isStickyNoteYes && isStickyNote)
         {
-            isStickyNote = false;
             isStickyNoteYes = true;
 
             stickyNote_original = stickyNote.gameObject;
@@ -179,6 +259,7 @@ public class Hero : MonoBehaviour
             training.GetComponent<Canvas>().enabled = true;
         }
 
+        // Закрываем стикер с обучением
         if (isStickyNote && Input.GetKeyDown(KeyCode.C))
         {
             training.GetComponent<Canvas>().enabled = false;
@@ -191,6 +272,7 @@ public class Hero : MonoBehaviour
             isStickyNoteYes = false;
         }
 
+        // Выбор: Что делать с карточкой? Выбросить, либо подобрать
         if (isChooseCardTakeOrDupe)
         {
             if (Input.GetKeyDown(KeyCode.F))
@@ -207,6 +289,8 @@ public class Hero : MonoBehaviour
                 isBoobaCanMove = true;
                 isBoobaCanJump = true;
                 isChooseCardTakeOrDupe = false;
+
+                saveCards();
             } 
             
             if (Input.GetKeyDown(KeyCode.C)) 
@@ -220,26 +304,30 @@ public class Hero : MonoBehaviour
             }
         }
 
-        // ask helper cactus (key press G) say about task1
+        // попросить помощи у Кактуса (нажав Z)
         if (isHelper && Input.GetKeyDown(KeyCode.Z))
         {
-            helperText.text = "Здарова, Леопольд";
-            Invoke("Hide", time);
+            // Проверка задания (currentTaskString)
+            // TODO Озвучка кактуса
         }
 
+        // Вход в N комнату
         if (isDoor && Input.GetKeyDown(KeyCode.X))
         {
             string nextRoom = room.gameObject.GetComponent<GoToDoor>().nextRoom;
             SceneManager.LoadScene(nextRoom);
         }
 
+        // Выход в меню
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("Menu");
 
+        // Вход в домик Бубы
         if (isDoorBooba && Input.GetKeyDown(KeyCode.X))
             SceneManager.LoadScene("homeBooba");
 
-        if (isTVOff && isRCB && day == 1 && Input.GetKeyDown(KeyCode.X))
+        // Включение/Выключенеи телевизора
+        if (isTVOff && isRCB && Input.GetKeyDown(KeyCode.X))
         {
             GameObject TVOff = GameObject.FindGameObjectWithTag("TVOff");
             AudioClip TVAudioSound = TV.gameObject.GetComponent<anySound>().sound;
@@ -249,7 +337,7 @@ public class Hero : MonoBehaviour
             TV.GetComponent<AudioSource>().Play();
 
             isTVOff = false;
-        } else if (!isTVOff && isRCB && day == 1 && Input.GetKeyDown(KeyCode.X)) 
+        } else if (!isTVOff && isRCB && Input.GetKeyDown(KeyCode.X)) 
         {
             GameObject TVOff = GameObject.FindGameObjectWithTag("TVOff");
 
@@ -259,16 +347,49 @@ public class Hero : MonoBehaviour
             isTVOff = true;
         }
 
+        // Буба ложится спать на свою кровать
         if (isBadBooba && Input.GetKeyDown(KeyCode.X))
         { 
+            newDay();
+
             GameObject bad = GameObject.FindGameObjectWithTag("badBooba");
             GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
 
             // bad.GetComponentInChildren<SpriteRenderer>().sprite = badWithBooba;
             bad.GetComponentInChildren<SpriteRenderer>().color = new Color(147/255f, 147/255f, 147/255f);
             cam.GetComponent<Camera>().backgroundColor = new Color(46/255f, 46/255f, 46/255f);
+        }
 
-            newDay();
+        // Триггеры в гараже
+        if (wireCounter < 4 && isWire && Input.GetKeyDown(KeyCode.X))
+        {
+            string provodType = wire.gameObject.GetComponent<CollectableScript>().itemType;
+            GameObject wireObj = GameObject.Find(provodType);
+            items.Add(provodType);
+            saveItems();
+
+            wireCounter++;
+            Destroy(wireObj);
+        }
+        if (wireCounter == 4 && isInstruments && Input.GetKeyDown(KeyCode.X))
+            isTakeInstuments = true;
+        if (isLightTrigger && Input.GetKeyDown(KeyCode.X))
+        {
+            string lightSprite = light.gameObject.GetComponent<CollectableScript>().itemType;
+            GameObject lightObj = GameObject.Find(lightSprite);
+            lightObj.GetComponent<SpriteRenderer>().sprite = newLight;
+            isLightOff = true;
+        }
+        if (isTakeInstuments && isLightOff && isMining && Input.GetKeyDown(KeyCode.X))
+        {
+            string miningType = mining.gameObject.GetComponent<CollectableScript>().itemType;
+            GameObject miningObj = GameObject.Find(miningType);
+            items.Add(miningType);
+            saveItems();
+
+            Destroy(miningObj);
+
+            currentTaskString = "Ванная";
         }
     }
 
@@ -281,43 +402,46 @@ public class Hero : MonoBehaviour
 
         // Сохраняем день
         PlayerPrefs.SetInt("Day", day);
-        Debug.Log(PlayerPrefs.GetInt("Day"));
-        
-        // Сохраняем собранные карточки
-        try {
-            PlayerPrefs.SetInt("cards_counter", collectsCards.Count);
-            for (int i = 0; i < collectsCards.Count; i++)
-                PlayerPrefs.SetString("collectsCards_list" + i, collectsCards[i]);
-        } catch { Debug.Log("Player has't get any cards now"); }
+        Debug.Log(day);
+    }
 
-        // Сохраняем собранные предметы
-        try {
-            PlayerPrefs.SetInt("items_counter", items.Count);
-            for (int i = 0; i < items.Count; i++)
-                PlayerPrefs.SetString("items_list" + i, items[i]);
-        } catch { Debug.Log("Player has't get any items now"); }
+    private void saveCards()
+    {
+        PlayerPrefs.SetInt("cards_counter", collectsCards.Count);
+        for (int i = 0; i < collectsCards.Count; i++)
+            PlayerPrefs.SetString("collectsCards_list" + i, collectsCards[i]);
+    }
+
+    private void saveItems()
+    {
+        PlayerPrefs.SetInt("cards_counter", collectsCards.Count);
+        for (int i = 0; i < collectsCards.Count; i++)
+            PlayerPrefs.SetString("collectsCards_list" + i, collectsCards[i]);
     }
 
     private void getSaves()
     {
-        PlayerPrefs.DeleteKey("Day");
-
-        try {
-            // Сохраняем день
-            day = PlayerPrefs.GetInt("Day");
+        // Сохраняем день
+        day = PlayerPrefs.GetInt("Day");
+        Debug.Log(day);
             
-            // Сохраняем собранные карточки
-            collectsCardsCount = PlayerPrefs.GetInt("cards_counter", collectsCards.Count);
-            for (int i = 0; i < collectsCards.Count; i++)
-                collectsCards.Add(PlayerPrefs.GetString("collectsCards_list" + i));
+        // Сохраняем собранные карточки
+        collectsCardsCount = PlayerPrefs.GetInt("cards_counter");
+        collectsCards = new List<string>();
 
-            // Сохраняем собранные предметы
-            itemsCount = PlayerPrefs.GetInt("items_counter", items.Count);
-            for (int i = 0; i < items.Count; i++)
-                items.Add(PlayerPrefs.GetString("items_list" + i));
-        } catch {
-            Debug.Log("Game has't saves");
-        }
+        for (int i = 0; i < collectsCardsCount; i++)
+            collectsCards.Add(PlayerPrefs.GetString("collectsCards_list" + i));
+
+        Debug.Log("Cards: " + collectsCards.Count);
+
+        // Сохраняем собранные предметы
+        itemsCount = PlayerPrefs.GetInt("items_counter");
+        items = new List<string>();
+
+        for (int i = 0; i < itemsCount; i++)
+            items.Add(PlayerPrefs.GetString("items_list" + i));
+
+        Debug.Log("Items: " + items.Count);
     }
 
     private void Run()
@@ -368,6 +492,14 @@ public class Hero : MonoBehaviour
         if (collision.CompareTag("RCB")) { TV = collision; isRCB = true; }
 
         if (collision.CompareTag("training")) { isStickyNote = true; stickyNote = collision; }
+        
+        if (collision.CompareTag("wires")) { isWire = true; wire = collision; }
+
+        if (collision.CompareTag("instruments")) { isInstruments = true; }
+
+        if (collision.CompareTag("mining")) { isMining = true; mining = collision; }
+
+        if (collision.CompareTag("light")) { isLightTrigger = true; light = collision; }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -385,6 +517,12 @@ public class Hero : MonoBehaviour
         TV = null;
         isBoobaCanMove = true;
         isBoobaCanJump = true;
+        isWire = false;
+        wire = null;
+        isInstruments = false;
+        isMining = false;
+        mining = null;
+        isLightOff = false;
     }
 }
 
