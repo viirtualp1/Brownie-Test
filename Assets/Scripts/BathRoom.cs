ed863f9ac889b3bd95dcba485f69d97f814a28d1
Nikita <zininnikita309@gmail.com>
2021-11-16 16:55:06 +0900

    okay, now
    - finished with item triggers for all days
    - made the animation of picking up an object

---
+++ Assets/Scripts/BathRoom.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathRoom : MonoBehaviour
+{
+    public Sprite new_Mirror;
+    public Sprite new_FishRod;
+}
